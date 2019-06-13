6fb05b9fee1202daaf04b7191446c7a1efaa442e
dengyanxiang <dengyanxiang@xiaomi.com>
2019-06-13 16:13:24 +0800

    fixbug

--- huawei/src/main/java/com/example/huawei/HuaWeiUtils.java
+++
@@ -1,6 +0,0 @@
-package com.example.huawei;
-
-// 用来处理华为出现问题的工具类
-public class HuaWeiUtils {
-
-}
--- room/src/main/java/com/example/room/worker/BlurViewModel.java
+++ room/src/main/java/com/example/room/worker/BlurViewModel.java
@@ -1,6 +1,5 @@
 package com.example.room.worker;
 
-import android.content.Context;
 import android.net.Uri;
 import android.text.TextUtils;
 
